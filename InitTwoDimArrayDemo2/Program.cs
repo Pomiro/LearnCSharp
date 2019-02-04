18baa042bf95abf22e7ef36dc254eec0e6ccb7c1
Popov Mikhail <pomiro2000@gmail.com>
2019-02-04 14:58:27 +0500

    v2

--- InitTwoDimArrayDemo2/Program.cs
+++ InitTwoDimArrayDemo2/Program.cs
@@ -1,15 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace InitTwoDimArrayDemo2
+class InitTwoDimArrayDemo
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        char[,] symbs = { { 'A', 'B', 'C' }, { 'D', 'E', 'F' } };
+
+        Console.WriteLine("Исходный массив:");
+
+        for (int i = 0; i < symbs.GetLength(0); i++)
         {
+            for (int j = 0; j < symbs.GetLength(1); j++)
+            {
+                Console.Write(symbs[i, j] + " ");
+            }
+            Console.WriteLine();
         }
+
+        Random rnd = new Random();
+        int row = rnd.Next(symbs.GetLength(0) + 1);
+        int col = rnd.Next(symbs.GetLength(0) + 1);
+        Console.WriteLine("Доболвяется {0}-строка и {1}-й столбец", row, col);
+
+        char[,] tmp = new char[symbs.GetLength(0) + 1, symbs.GetLength(1) + 1];
+
+        int a, b;
+        char s = 'a';
+
+        for (int i = 0; i < symbs.GetLength(0); i++)
+        {
+            if (i < row) a = i;
+            else a = i + 1;
+            for (int j = 0; j < symbs.GetLength(1); j++)
+            {
+                if (j < col) b = j;
+                else b = j + 1;
+                tmp[a, b] = symbs[i, j];
+            }
+        }
+        for (int j = 0; j < tmp.GetLength(1); j++)
+        {
+            tmp[row, j] = s;
+            s++;
+        }
+        for (int i = 0; i < tmp.GetLength(0); i++)
+        {
+            if (i != row)
+            {
+                tmp[i, col] = s;
+                s++;
+            }
+        }
+        symbs = tmp;
+        Console.WriteLine("Новый массив:");
+        for (int i = 0; i < symbs.GetLength(0); i++)
+        {
+            for (int j = 0; j < symbs.GetLength(1); j++)
+            {
+                Console.Write(symbs[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
     }
 }
+
